aa59e71fff19c8071ac5b4a3f30af1b645392467
Sümeyra Sarıgül <sumeyrasarigull@gmail.com>
2022-01-04 21:05:14 +0300

    Cube Avoid level design

---
+++ Assets/Scripts/CubeAvoid/CharacterGenerate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGenerate : MonoBehaviour
+{
+    public GameObject[] characterSelect;
+    public int charNum;
+    void Start()
+    {
+        charNum=Random.Range(0,3);
+        characterSelect[charNum].SetActive(true);
+    }
+}
